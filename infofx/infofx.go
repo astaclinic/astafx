37b9ebd053f4d884a8e4a9d864d1d79143c0e90e
Tommy Li <tommyli@asta.hk>
2022-11-29 16:44:20 +0800

    fix: remove extra newline in info

--- infofx/infofx.go
+++ infofx/infofx.go
@@ -20,16 +20,16 @@ type InfoParams struct {
 func DisplayInfo(p InfoParams) {
 	p.Lifecycle.Append(fx.Hook{
 		OnStart: func(context.Context) error {
-			p.Logger.Infof("%s\t[INFO]\tStarting application: \n", time.Now().Format(time.RFC3339))
-			p.Logger.Infof("%s\t[INFO]\t Golang Version: %v \n", time.Now().Format(time.RFC3339), runtime.Version())
-			p.Logger.Infof("%s\t[INFO]\t       Platform: %v %v \n", time.Now().Format(time.RFC3339), runtime.GOOS, runtime.GOARCH)
+			p.Logger.Infof("%s\t[INFO]\tStarting application:", time.Now().Format(time.RFC3339))
+			p.Logger.Infof("%s\t[INFO]\t Golang Version: %v", time.Now().Format(time.RFC3339), runtime.Version())
+			p.Logger.Infof("%s\t[INFO]\t       Platform: %v %v", time.Now().Format(time.RFC3339), runtime.GOOS, runtime.GOARCH)
 			hostname, err := os.Hostname()
 			if err != nil {
 				hostname = fmt.Sprintf("Fail to get hostname: %v", err)
 			}
-			p.Logger.Infof("%s\t[INFO]\t       Hostname: %v\n", time.Now().Format(time.RFC3339), hostname)
-			p.Logger.Infof("%s\t[INFO]\t     Build Date: %v\n", time.Now().Format(time.RFC3339), os.Getenv("BUILD_DATE"))
-			p.Logger.Infof("%s\t[INFO]\t   Build Commit: %v\n", time.Now().Format(time.RFC3339), os.Getenv("BUILD_COMMIT"))
+			p.Logger.Infof("%s\t[INFO]\t       Hostname: %v", time.Now().Format(time.RFC3339), hostname)
+			p.Logger.Infof("%s\t[INFO]\t     Build Date: %v", time.Now().Format(time.RFC3339), os.Getenv("BUILD_DATE"))
+			p.Logger.Infof("%s\t[INFO]\t   Build Commit: %v", time.Now().Format(time.RFC3339), os.Getenv("BUILD_COMMIT"))
 			return nil
 		},
 	})
