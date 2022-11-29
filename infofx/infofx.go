8f4e98085994c47f0bd4adc9998eaf1a723e3969
Tommy Li <tommyli@asta.hk>
2022-11-29 16:50:30 +0800

    fix: remove extra timestamp argument

--- infofx/infofx.go
+++ infofx/infofx.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os"
 	"runtime"
-	"time"
 
 	"go.uber.org/fx"
 	"go.uber.org/zap"
@@ -20,16 +19,16 @@ type InfoParams struct {
 func DisplayInfo(p InfoParams) {
 	p.Lifecycle.Append(fx.Hook{
 		OnStart: func(context.Context) error {
-			p.Logger.Infof("Starting application:", time.Now().Format(time.RFC3339))
-			p.Logger.Infof(" Golang Version: %v", time.Now().Format(time.RFC3339), runtime.Version())
-			p.Logger.Infof("       Platform: %v %v", time.Now().Format(time.RFC3339), runtime.GOOS, runtime.GOARCH)
+			p.Logger.Info("Starting application:")
+			p.Logger.Infof(" Golang Version: %v", runtime.Version())
+			p.Logger.Infof("       Platform: %v %v", runtime.GOOS, runtime.GOARCH)
 			hostname, err := os.Hostname()
 			if err != nil {
 				hostname = fmt.Sprintf("Fail to get hostname: %v", err)
 			}
-			p.Logger.Infof("       Hostname: %v", time.Now().Format(time.RFC3339), hostname)
-			p.Logger.Infof("     Build Date: %v", time.Now().Format(time.RFC3339), os.Getenv("BUILD_DATE"))
-			p.Logger.Infof("   Build Commit: %v", time.Now().Format(time.RFC3339), os.Getenv("BUILD_COMMIT"))
+			p.Logger.Infof("       Hostname: %v", hostname)
+			p.Logger.Infof("     Build Date: %v", os.Getenv("BUILD_DATE"))
+			p.Logger.Infof("   Build Commit: %v", os.Getenv("BUILD_COMMIT"))
 			return nil
 		},
 	})
