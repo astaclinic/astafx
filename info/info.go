2827219c35c12cff16b6351add0be279d8abfbf5
Michael Chin <michaelchin@asta.hk>
2023-03-03 10:50:47 +0800

    remove redundant new line

--- info/info.go
+++ info/info.go
@@ -24,15 +24,15 @@ type InfoDisplay struct {
 
 func GetInfo() (*InfoDisplay, error) {
 	display := &InfoDisplay{
-		Name:     fmt.Sprintf("        Program: %v\n", ProgramName),
-		Platform: fmt.Sprintf("       Platform: %v %v\n", runtime.GOOS, runtime.GOARCH),
-		Runtime:  fmt.Sprintf("        Runtime: %v\n", runtime.Version()),
+		Name:     fmt.Sprintf("        Program: %v", ProgramName),
+		Platform: fmt.Sprintf("       Platform: %v %v", runtime.GOOS, runtime.GOARCH),
+		Runtime:  fmt.Sprintf("        Runtime: %v", runtime.Version()),
 	}
 	hostname, err := os.Hostname()
 	if err != nil {
 		hostname = fmt.Sprintf("Fail to get hostname: %v", err)
 	}
-	display.HostName = fmt.Sprintf("       Hostname: %v\n", hostname)
+	display.HostName = fmt.Sprintf("       Hostname: %v", hostname)
 	buildInfo, ok := debug.ReadBuildInfo()
 	if !ok {
 		return nil, errors.New("failed to read build info")
@@ -43,7 +43,7 @@ func GetInfo() (*InfoDisplay, error) {
 			buildCommit = buildSetting.Value
 		}
 	}
-	display.BuildCommit = fmt.Sprintf("   Build Commit: %v\n", buildCommit)
+	display.BuildCommit = fmt.Sprintf("   Build Commit: %v", buildCommit)
 	display.BuildDate = fmt.Sprintf("     Build Date: %v", BuildDate)
 	return display, nil
 }
