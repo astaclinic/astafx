7354302505b706823a3255935dd6a45706fd7f87
Michael Chin <michaelchin@asta.hk>
2023-03-06 16:20:08 +0800

    change of dbfx config

--- dbfx/db.go
+++ dbfx/db.go
@@ -10,7 +10,11 @@ import (
 )
 
 type PostgresConfig struct {
-	Dsn string `mapstructure:"dsn" yaml:"dsn" validate:"required,uri"`
+	UserName string `mapstructure:"user_name" yaml:"user_name" validate:"required"`
+	Password string `mapstructure:"password" yaml:"password" validate:"required"`
+	Host     string `mapstructure:"host" yaml:"host" validate:"required"`
+	Database string `mapstructure:"database" yaml:"database" validate:"required"`
+	Port     string `mapstructure:"port" yaml:"port" validate:"required"`
 }
 
 func init() {
@@ -26,7 +30,8 @@ type Params struct {
 }
 
 func New(p Params) (*gorm.DB, error) {
-	db, err := gorm.Open(postgres.Open(p.Config.Dsn), &gorm.Config{
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", p.Config.UserName, p.Config.Password, p.Config.Host, p.Config.Port, p.Config.Database)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: p.GormLogger,
 	})
 	if err != nil {
